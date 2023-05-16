4a9e1bbbab6b8eba3f75eeb64e69abfacdae684b
brendanmac001 <atlasmaccc@gmail.com>
2023-05-16 12:12:39 -0400

    chnages

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,7 +7,7 @@ import Show from '../pages/Show'
 const Main = (props) => {
     const [ people, setPeople ] = useState(null)
 
-    const URL = "https://er-backend.onrender.com"
+    const URL = "https://er-backend.onrender.com/people/"
 
     const getPeople = async () => {
         const response = await fetch(URL)
